550fee3f001f4757da8d22b8e9c661de0655f0e0
mariliafernandez <marilia.mfernandez@gmail.com>
2020-05-31 23:00:59 -0300

    Find the best parameters to SVM classifier

--- main.py
+++ main.py
@@ -5,8 +5,29 @@ from itertools import repeat
 import glob
 import os
 from sklearn.metrics import precision_score, accuracy_score, mean_squared_error
-from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+def best_parameters(X, y):
+    C_range = np.logspace(-2, 10, 13)
+    gamma_range = np.logspace(-9, 3, 13)
+    param_grid = dict(gamma=gamma_range, C=C_range)
+    cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
+    grid = GridSearchCV(SVC(), param_grid=param_grid, cv=cv)
+    grid.fit(X, y)
+    return grid.best_params_
+
+def print_metrics(classifier, y_test, predicted, class_names):
+    print("Classification report for classifier %s:\n%s\n"
+        % (classifier, metrics.classification_report(y_test, predicted, target_names=class_names)))
+
+    print('Confusion Matrix')
+    print( metrics.confusion_matrix(y_test, predicted) )
+
 
 # Classes
 left_paths = []
@@ -19,13 +40,12 @@ sad_paths = []
 angry_paths = []
 neutral_paths = []
 
-eyes_open_paths = []
-sunglass_paths = []
+open_paths = []
+sunglasses_paths = []
 
 # Listing paths of files for each class
 for folder in os.listdir('faces_4'):
     # head position
-    # left_paths += np.array(Image.open('faces_4/' + folder + '/*left*.pgm'))
     left_paths += glob.glob('faces_4/' + folder + '/*left*.pgm')
     right_paths += glob.glob('faces_4/' + folder + '/*right*.pgm')
     straight_paths += glob.glob('faces_4/' + folder + '/*straight*.pgm')
@@ -38,11 +58,11 @@ for folder in os.listdir('faces_4'):
     neutral_paths += glob.glob('faces_4/' + folder + '/*neutral*.pgm')
 
     # eyes
-    eyes_open_paths += glob.glob('faces_4/' + folder + '/*eyes_open*.pgm')
-    sunglass_paths += glob.glob('faces_4/' + folder + '/*sunglass*.pgm')
+    open_paths += glob.glob('faces_4/' + folder + '/*open*.pgm')
+    sunglasses_paths += glob.glob('faces_4/' + folder + '/*sunglasses*.pgm')
 
-filenames = []
-filenames.extend([left_paths, right_paths, straight_paths, up_paths, happy_paths, sad_paths, angry_paths, neutral_paths, eyes_open_paths, sunglass_paths])
+paths = []
+paths.extend([left_paths, right_paths, straight_paths, up_paths, happy_paths, sad_paths, angry_paths, neutral_paths, open_paths, sunglasses_paths])
 
 left = []
 right = []
@@ -52,27 +72,21 @@ happy = []
 sad = []
 angry = []
 neutral = []
-eyes_open = []
-sunglass = []
+_open = []
+sunglasses = []
 
 classes = []
-classes.extend([left, right, straight, up, happy, sad, angry, neutral, eyes_open, sunglass])
+classes.extend([left, right, straight, up, happy, sad, angry, neutral, _open, sunglasses])
 i=0
-for category in filenames:
+for category in paths:
     for filename in category:
-        img = Image.open(filename)
-        # PpmImageFile to Numpy Array
-        classes[i].append( np.array(img).flatten())
-        img.close()
+        img = plt.imread(filename)
+        classes[i].append( img.flatten())
     i+=1
-
-# for item in left_paths:
-#     left.append(Image.open(item))
-
 
 head_x = left + right + straight + up
 emotion_x = happy + sad + angry + neutral
-eyes_x = eyes_open + sunglass
+eyes_x = _open + sunglasses
 
 head_y = []
 head_y.extend(repeat(0, len(left)))
@@ -87,27 +101,56 @@ emotion_y.extend(repeat(2, len(angry)))
 emotion_y.extend(repeat(3, len(neutral)))
 
 eyes_y = []
-eyes_y.extend(repeat(0, len(eyes_open)))
-eyes_y.extend(repeat(1, len(sunglass)))
-
-features = []
-features.extend([head_x, emotion_x, eyes_x])
+eyes_y.extend(repeat(0, len(_open)))
+eyes_y.extend(repeat(1, len(sunglasses)))
 
 # Splitting train and test datasets
 head_x_train, head_x_test, head_y_train, head_y_test = train_test_split(head_x, head_y, test_size = 0.3)
 emotion_x_train, emotion_x_test, emotion_y_train, emotion_y_test = train_test_split(emotion_x, emotion_y, test_size=0.3)
 eyes_x_train, eyes_x_test, eyes_y_train, eyes_y_test = train_test_split(eyes_x, eyes_y, test_size=0.3)
 
-classifier = SVC(gamma = 0.001)
-classifier.fit(head_x_train, head_y_train)
+features_dic = {'head' : [ head_x_train, head_x_test, head_y_train, head_y_test ],
+                'emotion' : [ emotion_x_train, emotion_x_test, emotion_y_train, emotion_y_test ],
+                'eyes' : [ eyes_x_train, eyes_x_test, eyes_y_train, eyes_y_test ]}
 
-y_predicted = classifier.predict(head_x_test)
+class_dic = {'head': ['left', 'right', 'straight', 'up'], 
+             'emotion' : ['happy', 'sad', 'angry', 'neutral'], 
+             'eyes' : ['open', 'sunglasses'] }
 
-print(y_predicted, head_y_test)
-print(y_predicted == head_y_test)
-# mse = mean_squared_error(head_y_test, y_predicted)
-# prec = precision_score(head_y_test, y_predicted, average='macro')
-# acc = accuracy_score(head_x_test, y_predicted)
-# print('mean squared error:', mse)
-# print('precision: ', prec)
-# print('accuracy: ', acc)
+# 
+class_names = ['happy', 'sad', 'angry', 'neutral']
+X = emotion_x_train
+X_test = emotion_x_test
+y = emotion_y_train
+y_test = emotion_y_test
+print('train')
+print(len(y))
+print('test')
+print(len(y_test))
+
+
+
+for key in features_dic:
+    class_names = class_dic[key]
+    feature = features_dic[key]
+    X = feature[0]
+    X_test = feature[1]
+    y = feature[2]
+    y_test = feature[3]
+
+    params = best_parameters(X,y)
+
+    pipe = Pipeline([('scaler', StandardScaler()), ('svc', SVC(C=params['C'], gamma=params['gamma'], kernel='linear', cache_size=500))])
+    pipe.fit(X,y)
+    predicted = pipe.predict(X_test)
+
+    print('predicted')
+    print(len(predicted))
+
+    print_metrics(pipe, y_test, predicted, class_names)
+
+
+# Parameters found
+# head position: C=10.0, gamma=1e-06
+# emotion: C=0.01, gamma=1e-09 (Bad results)
+# eyes: C=10000.0, gamma=1e-09 
